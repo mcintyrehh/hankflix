891b995699e0352b3c8daed17ee7a9004be36b8b
Henry <mcintyrehh@gmail.com>
2019-01-15 21:30:44 -0500

    connected backend into front end, mongodb server up and running

---
+++ server/server.js
@@ -0,0 +1,60 @@
+// const scrapeController = require("./controllers/scrapeController");
+
+// Loading environmental variables here
+if (process.env.NODE_ENV !== 'production') {
+	console.log('loading dev environments');
+	// require('dotenv').config();
+}
+// require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dbConnection = require('./db'); // loads our connection to the mongo database
+const routes = require("./routes");
+// const passport = require('./passport');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(session({
+  secret: process.env.APP_SECRET || 'this is the default passphrase',
+  store: new MongoStore({ mongooseConnection: dbConnection }),
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport
+// app.use(passport.initialize());
+// app.use(passport.session()); // will call the deserializeUser
+
+
+// If its production environment!
+if (process.env.NODE_ENV === 'production') {
+	console.log('YOU ARE IN THE PRODUCTION ENV');
+	app.use(express.static(path.join(__dirname, '../', 'client/build')));
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Error handler
+app.use(function(err, req, res, next) {
+	console.log('====== ERROR =======');
+	console.error(err.stack);
+	res.status(500);
+});
+
+
+// Starting Server
+app.listen(PORT, () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
