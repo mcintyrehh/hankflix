362733574be3801425c3af4ae8ade61ee272c7b9
Henry <mcintyrehh@gmail.com>
2019-03-01 12:19:09 -0500

    fixing routing to display the react app if no routes are hit

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,14 @@ var router = express.Router();
 const authRoutes = require("./auth");
 const apiRoutes = require("./api");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 // Auth Routes
 router.use('/auth', authRoutes);
 
 //API Routes
 router.use("/api", apiRoutes);
 
+// If no routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 module.exports = router;
